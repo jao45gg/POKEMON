a571bca3df20aed7bee28ceb14f293dd36f04f95
thaismadeira10 <thaismadeira2003@gmail.com>
2022-11-17 20:20:44 -0300

    classe cartas
    foi criado a classe cartas e os headers de efeitos e habilidades.

---
+++ include/Efeitos.hpp
@@ -0,0 +1,13 @@
+#ifndef _EFEITOS_HPP_
+#define _EFEITOS_HPP_
+
+Class Efeitos{
+
+  protected:
+  string _nome_efeito;
+  public:
+  Efeitos();
+  
+};
+
+#endif
---
+++ include/Habilidades.hpp
@@ -0,0 +1,11 @@
+#ifndef _HABILIDADES_HPP_
+#define _HABILIDADES_HPP_
+
+Class Habilidades{
+
+  public:
+  Habilidades();
+};
+
+
+#endif

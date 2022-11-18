7867f69a9cea9a2eba55f719dac5d7f53a1e0ec9
Gabriel Hesse Vitusso <53229154+gabriezim@users.noreply.github.com>
2022-11-18 19:34:35 -0300

    Update Habilidades.hpp

--- include/Habilidades.hpp
+++ include/Habilidades.hpp
@@ -1,11 +1,24 @@
 #ifndef _HABILIDADES_HPP_
 #define _HABILIDADES_HPP_
 
-Class Habilidades{
+#include "Efeitos.hpp"
 
-  public:
-  Habilidades();
+#include <iostream>
+
+
+using namespace std;
+
+class Habilidades{
+private:
+    int _tempoRecarga;
+    Efeitos _efeito;
+protected:
+    string _nome;
+public:
+    Habilidades(int tempoRecarga, string nome, Efeitos efeito);
+    ~Habilidades();
+    Efeitos* getEfeito();
+    int gettempoRecarga();
 };
-
 
 #endif

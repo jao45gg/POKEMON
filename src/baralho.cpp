c35677ccf447d1913b92c9d2fc3ae9eb198099f9
uchoaluiz <lotaviouchoa@gmail.com>
2022-11-28 16:01:40 -0300

    Baralho

--- src/baralho.cpp
+++ src/baralho.cpp
@@ -1,5 +1,5 @@
-#ifndef BARALHO_CPP
-#define BARALHO_CPP
+#ifndef _BARALHO_CPP
+#define _BARALHO_CPP
 
 #include "Baralho.hpp"
 #include "Cartas.hpp"
@@ -8,21 +8,66 @@
 
 using namespace std;
 
-Baralho::Baralho(vector <Cartas> cartasP, string nomeP){
-    this->_cartas = cartasP;
+Baralho::Baralho(vector<Cartas> &cartasP, string nomeP, vector<Cartas> &cartasExistentes)
+{
     this->_nome = nomeP;
+    while(((this->_cartas).size()) <= 60){
+        string nomeCarta;
+        cin >> nomeCarta;
+        bool existeCarta = false;
+        for(int j = 0, je = (cartasExistentes.size()); j < je; j++){
+            string *procuraNome = ((cartasExistentes.at(j)).getNome());
+            if(nomeCarta == *procuraNome){
+                (this->_cartas).push_back(cartasExistentes.at(j));
+                existeCarta = true;
+                break;
+            }
+        }
+        if(!existeCarta){
+            cout << "Nao existe carta com esse nome" << endl;
+        }
+    }
 };
 
-Baralho::~Baralho(){
+Baralho::~Baralho()
+{
     (this->_cartas).clear();
     this->_nome = "";
 };
 
-string Baralho::getNome(){
+string Baralho::getNome()
+{
     return this->_nome;
 };
 
-void Baralho::setNome(string nomeP){
+void Baralho::setNome(string nomeP)
+{
     this->_nome = nomeP;
 };
+
+Cartas Baralho::getCarta(int num)
+{
+    for (int i = 0, ie = _cartas.size(); i < ie; i++)
+    {
+        if (i == num)
+            return _cartas[i];
+    }
+};
+
+void Baralho::removeCarta(string nome) {
+    bool control = true;
+    int ie = 0;
+    for(auto i = _cartas.begin(); control; i++) {
+        if(_cartas.at(ie).getNome() == nome) {
+            _cartas.erase(i);
+            break;
+        }
+        ie++;     
+    }
+}
+
+int Baralho::getSize() {
+    return _cartas.size();
+}
+
 #endif

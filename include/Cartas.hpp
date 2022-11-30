3afd65e09f1c8aca29fe10aa7adece23a2c9e048
Gabriel Hesse Vitusso <53229154+gabriezim@users.noreply.github.com>
2022-11-29 21:26:42 -0300

    Comentarios
    Realizei a confecção de comentários que estarão presentes no código, ainda devo passar para os cpp's, adiei isso para facilitar modificações nessas partes

--- include/Cartas.hpp
+++ include/Cartas.hpp
@@ -8,6 +8,7 @@ using namespace std;
 class Cartas
 {
 private:
+//Variaveis
   string *_nome_pokemon;
   int *_ataque;
   int *_defesa;
@@ -17,17 +18,78 @@ private:
   bool cartaEnergiaUsada;
 
 public:
+  /**
+   * @brief Construtor para a classe Cartas, 
+   * faz atribuicoes iniciais de variaveis.
+   * @param nome Nome atribuido aa carta
+   * @param ataque Atributo do dano que a carta realiza
+   * @param defesa Atributo do modificador do dano recebido
+   * @param tipo Atributo de modificador do dano realizado
+   * @param hp Atributo de dano que pode ser recebido
+   * 
+   */
   Cartas(string nome, int ataque, int defesa, string tipo,
          int hp);
+  /**
+   * @brief Construtor ad-hoc para a classe, 
+   * somente determina a atribuicao de tipo.
+   * @param tipo Atributo do modificador do dano recebido
+   * 
+   */
   Cartas(string tipo);
+  /**
+   * @brief Destrutor para a classe Cartas.
+   * Libera memoria alocada dinamicamente.
+   * 
+   */
   ~Cartas();
+  /**
+   * @brief Funcao que retorna nome
+   * @return Nome da carta
+   */
   string getNome();
+  /**
+   * @brief Funcao que retorna ataque
+   * @return Ataque da carta
+   * 
+   */
   int getAtaque();
+  /**
+   * @brief Funcao que retorna defesa
+   * @return Defesa da carta
+   */
   int getDefesa();
+  /**
+   * @brief Funcao que retorna tipo
+   * @return Tipo da carta
+   * 
+   */
   string getTipo();
+  /**
+   * @brief Funcao que retorna Hp da carta
+   * @return Hp da carta
+   * 
+   */
   int getHp();
+  /**
+   * @brief Funcao que associa uma carta de energia
+   * a uma carta de pokemon
+   * @param cartaEnergia ponteiro Cartas que leva aa carta de energia
+   * @param pokemon ponteiro Cartas que leva aa carta do pokemon
+   * 
+   */
   void ligarEnergia(Cartas *cartaEnergia, Cartas *pokemon);
+  /**
+   * @brief Funcao que altera o Hp da Carta em questao
+   * @param dano Dano recebido
+   * 
+   */
   void sofrerDano(int dano);
+  /**
+   * @brief Funcao que retorna a energia da carta
+   * @return bool que determina o status da energia
+   * 
+   */
   bool getEnergia();
 };
 
--- include/Partida.hpp
+++ include/Partida.hpp
@@ -9,6 +9,7 @@ using namespace std;
 class Partida
 {
 private:
+//Variaveis
     Usuario *_jogador;
     Usuario *_bot;
     Baralho *_baralhoJogador;
@@ -18,10 +19,42 @@ private:
     string _nomeBaralhoJogador, _nomeBaralhoBot;
 
 public:
+//Metodos
+    /**
+     * @brief Construtor da classe Partida, 
+     * inicializa e atribui as variaveis
+     * @param jogador ponteiro do usuario que ira jogar
+     * @param bot ponteiro do usuario(bot) que ira jogar
+     * @param nomeBaralhoJogador string do nome do baralho jogado
+     * @param nomeBaralhoBot string do nome do baralho adversario
+     */
     Partida(Usuario *jogador, Usuario *bot, string nomeBaralhoJogador, string nomeBaralhoBot);
+    /**
+     * @brief Destrutor para a classe Partida, desaloca a memoria dinamica
+     * 
+     */
     ~Partida();
+    /**
+     * @brief Faz com que o atacante compre uma carta durante a partida
+     * @param atacante ponteiro que indica o jogador que fez a acao
+     * 
+     */
     void comprarCarta(Usuario *atacante);
+    /**
+     * @brief Faz com que o atacante realize um ataque
+     * @param cartaAtacando ponteiro da carta que ataca
+     * @param cartaAtacada ponteiro da carta que sofre o dano
+     * @param atacante ponteiro do usuario que faz a acao
+     * 
+     */
     void Ataque(string *cartaAtacando, string *cartaAtacada, Usuario *atacante);
+    /**
+     * @brief Atribui uma carta de energia a uma carta de pokemon
+     * @param _cartaEnergia ponteiro da carta de energia
+     * @param _pokemon ponteiro da carta de pokemon
+     * @param atacante ponteiro do usuario que faz a acao
+     * 
+     */
     void _ligarEnergia(Cartas *_cartaEnergia, Cartas *_pokemon, Usuario *atacante);
 };
 

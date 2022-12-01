1a806f2a7be71810d07a9215be3f7216332d5b3d
Gabriel Hesse Vitusso <53229154+gabriezim@users.noreply.github.com>
2022-12-01 12:26:16 -0300

    Update Partida.hpp

--- include/Partida.hpp
+++ include/Partida.hpp
@@ -22,11 +22,52 @@ private:
     string _nomeBaralhoJogador, _nomeBaralhoBot;
 
 public:
+
+    /**
+     * @brief Construtor de partidas, inicializa classe
+     * e atribui os nomes de jogadores e Usuarios
+     * @param jogador Ponteiro de Usuario dado ao jogador
+     * @param bot Ponteiro de Usuario dado ao computador
+     * @param nomeBaralhoJogador Nome do baralho
+     * @param nomeBaralhoBot Nome do baralho do Bot
+     * 
+     */
     Partida(Usuario *jogador, Usuario *bot, string nomeBaralhoJogador, string nomeBaralhoBot);
+    
+     /**
+     * @brief Destrutor para a classe Partida,
+     * desaloca a memoria dinamica
+     * 
+     */
     ~Partida();
+    
+    /**
+     * @brief Faz com que o atacante compre uma carta durante a partida
+     * @param atacante ponteiro que indica o jogador que fez a acao
+     * 
+     */
     void comprarCarta(Usuario *atacante);
-    void Ataque(string *cartaAtacando, string *cartaAtacada, Usuario *atacante);       // ataque generico
+    
+     /**
+     * @brief Faz com que o atacante realize um ataque
+     * @param cartaAtacando ponteiro da carta que ataca
+     * @param cartaAtacada ponteiro da carta que sofre o dano
+     * @param atacante ponteiro do usuario que faz a acao
+     * 
+     */
+    void Ataque(string *cartaAtacando, string *cartaAtacada, Usuario *atacante); // ataque generico
+    
+     /**
+     * @brief Determina o dano de uma habilidade
+     * @param _atacando Ponteiro que indica a carta que ataca
+     * @param _defendendo ponteiro que indica a carta que defende
+     * @param multiplicador_dano atributo que altera o dano aplicado
+     * @param infos Vector de informacao de tipos
+     * 
+     */
     void ataqueTipo(Cartas *_atacando, Cartas *_defendendo, float multiplicador_dano); // ataque especifico de cada tipo de pokemon
+    
+    
     void _ligarEnergia(Cartas *_cartaEnergia, Cartas *_pokemon, Usuario *atacante);
 };
 

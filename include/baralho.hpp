688f4338d9bc44e893cc6d986b34a5710209edc2
Gabriel Hesse Vitusso <53229154+gabriezim@users.noreply.github.com>
2022-11-30 22:05:25 -0300

    Add files via upload

--- include/baralho.hpp
+++ include/baralho.hpp
@@ -10,21 +10,87 @@ using namespace std;
 class Baralho
 {
 private:
-
+//Variaveis
     vector<Cartas> _cartas;
     string _nome;
-
+//Metodos
 public:
-
+    /**
+     * @brief Construtor simples para Baralho,
+     * realiza atribuicoes genericas para o preparo
+     * do uso
+     * 
+     */
     Baralho();
+
+    /**
+     * @brief Construtor que faz as atribuicoes iniciais
+     * das variaveis contidas na classe Baralho.
+     * @param cartasExistentes Endereco do vetor de cartas que sera salvo
+     * @param nomeP String que equivale ao nome em que o baralho
+     * eh salvo
+     * 
+     */
     Baralho(string nomeP, vector<Cartas> &cartasExistentes);
+
+    /**
+    * @brief Destrutor da classe, desaloca a memoria dinamica.
+    * 
+    */
     ~Baralho();
+
+    /**
+    * @brief Funcao que retorna o nome do baralho
+    * @return Nome do Baralho
+    * 
+    */
     string getNome();
+    
+    /**
+     * @brief Funcao que retorna um vetor de
+     * cartas do baralho
+     * @return Vector contendo todas cartas
+     * baralho
+     * 
+     */
     vector<Cartas> getCartasBaralho();
+
+    /**
+     * @brief Funcao que iguala os conteudos
+     * de um baralho aos de outro baralho
+     * @param operando Endereco do baralho em que
+     * os seus conteudos serao igualados aos conteudos
+     * do primeiro baralho (baralho = operando)
+     * 
+     */
     void operator=(Baralho &operando);
+
+    /**
+     * @brief Funcao que altera o nome do baralho
+     * @param nomeP String que equivale ao novo nome
+     * 
+     */
     void setNome(string nomeP);
+
+    /**
+     * @brief Funcao que retorna um numero equivalente a uma carta
+     * @return "Id" ou numero de representacao de uma carta
+     * 
+     */
     Cartas getCarta(int num);
+
+    /**
+     * @brief Funcao que remove a carta escolhida do baralho
+     * @param nome Nome da carta que sera removida
+     * 
+     */
     void removeCarta(string nome);
+    /**
+     * @brief Funcao que retorna o valor inteiro da quantidade
+     * de cartas
+     * @return Int de quantidade de cartas no baralho
+     * 
+     */
     int getSize();
 };
 #endif

e9a1dfd13e88706c72fe9c83b585ef76132d73c7
Gabriel Hesse Vitusso <53229154+gabriezim@users.noreply.github.com>
2022-12-01 12:29:57 -0300

    Update Partida.hpp

--- include/Partida.hpp
+++ include/Partida.hpp
@@ -28,13 +28,72 @@ private:
 
 public:
 
+    /**
+     * @brief Construtor de partidas, inicializa classe
+     * e atribui os nomes de jogadores e Usuarios
+     * @param jogador Ponteiro de Usuario dado ao jogador
+     * @param bot Ponteiro de Usuario dado ao computador
+     * @param nomeBaralhoJogador Nome do baralho
+     * @param nomeBaralhoBot Nome do baralho do Bot
+     * 
+     */
     Partida(Usuario jogador, Usuario bot, string nomeBaralhoJogador, string nomeBaralhoBot);
+    
+     /**
+     * @brief Destrutor para a classe Partida,
+     * desaloca a memoria dinamica
+     * 
+     */
     ~Partida();
+    
+     /**
+     * @brief Faz com que o atacante compre uma carta durante a partida
+     * @param atacante ponteiro que indica o jogador que fez a acao
+     * 
+     */
     void comprarCarta(Usuario *atacante);
-    void Ataque(string *cartaAtacando, string *cartaAtacada, Usuario *atacante);       // ataque generico
+    
+     /**
+     * @brief Faz com que o atacante realize um ataque
+     * @param cartaAtacando ponteiro da carta que ataca
+     * @param cartaAtacada ponteiro da carta que sofre o dano
+     * @param atacante ponteiro do usuario que faz a acao
+     * 
+     */
+    void Ataque(string *cartaAtacando, string *cartaAtacada, Usuario *atacante);// ataque generico
+    
+     /**
+     * @brief Determina o dano de uma habilidade
+     * @param _atacando Ponteiro que indica a carta que ataca
+     * @param _defendendo ponteiro que indica a carta que defende
+     * @param multiplicador_dano atributo que altera o dano aplicado
+     * @param infos Vector de informacao de tipos
+     * 
+     */
     //void ataqueTipo(Cartas *_atacando, Cartas *_defendendo, float multiplicador_dano); //, pair<string,int> infos); // ataque especifico de cada tipo de pokemon
+    
+     /**
+     * @brief Atribui uma carta de energia a uma carta de pokemon
+     * @param _cartaEnergia ponteiro da carta de energia
+     * @param _pokemon ponteiro da carta de pokemon
+     * @param atacante ponteiro do usuario que faz a acao
+     * 
+     */    
     void _ligarEnergia(Cartas *_cartaEnergia, Cartas *_pokemon, Usuario *atacante);
+    
+    /**
+     * @brief Funcao que determina as condicoes de parada da partida
+     * @param baralho ponteiro do baralho usado na comparacao
+     * @param morto ponteiro da carta que acabara de morrer
+     * 
+     */    
     void pokemonMorto(Baralho *baralho, Cartas *morto);
+    
+     /**
+     * @brief Funcao que devolve as cartas presentes na mao do jogador
+     * @param ponteiro do Usuario que escolheu ver a mao
+     * 
+     */   
     void exibirMao(Usuario *mao);
 };
 
